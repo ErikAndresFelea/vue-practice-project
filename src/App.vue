a35dc4e21df8869acd4cae907e4314a9c1fc60a0
Erik Andres Felea <erikandresfelea@gmail.com>
2022-12-23 09:10:39 +0100

    implemented db on json

--- src/App.vue
+++ src/App.vue
@@ -43,28 +43,19 @@ export default {
         task.id === id ? { ...task, reminder: !task.reminder } : task
       )
     },
+    async fetchTasks() {
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json()
+      return data
+    },
+    async fetchTask(id) {
+      const res = await fetch(`http://localhost:5000/tasks${id}`)
+      const data = await res.json()
+      return data
+    },
   },
-  created() {
-    this.tasks = [
-      {
-        id: 1,
-        text: 'Doctors appointment',
-        day: 'March 1st at 2:30pm',
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: 'Meeting at school',
-        day: 'March 3rd at 1:30pm',
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: 'Food shopping',
-        day: 'March 3rd at 11:00am',
-        reminder: false,
-      },
-    ]
+  async created() {
+    this.tasks = await this.fetchTasks()
   },
 }
 </script>
